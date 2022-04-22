57c0bafc3094dec1d48e27f8bda36a0eb9893ddc
yangqing <yangqing@jiunidu.com>
2022-04-22 15:20:52 +0800

    version 1.0.16

--- utils/request.js
+++ utils/request.js
@@ -8,9 +8,9 @@
 //var apiurl = 'http://localhost:8080/flash20AppletBackend'         // 本地测试
 // var apiurl = 'https://store.coyotebio-lab.com:8443/lis_appointment'    // 测试服务器
 //var apiurl = 'http://xmr.coyotebio-lab.com/lis_appointment'    // 测试服务器
-//var apiurl = 'https://cloud.coyotebio-lab.com:8443/lis_appointment'    // 服务器
+var apiurl = 'https://cloud.coyotebio-lab.com:8443/lis_appointment'    // 服务器
 //8.130.48.31
-var apiurl = 'https://scldev.coyotebio-lab.com/lis_appointment'//测试服务器 孙仕豪
+// var apiurl = 'https://scldev.coyotebio-lab.com/lis_appointment'//测试服务器 孙仕豪
 //var apiurl = 'http://8.130.25.5:8080/flash20AppletBackend' // 测试服务器 宋彦睿
 //var apiurl = 'https://syrdev.coyotebio-lab.com/flash20AppletBackend/'
 //var apiurl = 'http://8.130.55.156:8080/flash20AppletBackend'    // 测试服务器  于光良
