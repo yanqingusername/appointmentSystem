ad98e2b3c489020fe5ad1bae6ef6c793cdef60ec
yangqing <yangqing@jiunidu.com>
2022-10-27 18:00:44 +0800

    调试我的优惠券接口 返回数据不全

--- healthyshop/pages/shoppingcoupon/index.js
+++ healthyshop/pages/shoppingcoupon/index.js
@@ -8,7 +8,9 @@ Page({
     number: 1,
     couponArr:[],
     user_id: '',
-    openid: ''
+    openid: '',
+
+    mineCouponList: []
   },
   onLoad: function (options) {
     this.setData({
@@ -16,46 +18,43 @@ Page({
       openid: wx.getStorageSync('coyote_userinfo').openid || '',
     });
 
-    var that = this
-    that.getCouponList()
-
+    this.getCouponList();
+    this.getMineCouponList();
   },
   getCouponList(){
-    var that = this
+    var that = this;
     var data = {
       openid:this.data.openid,
     }
     request.request_get('/activity/getCouponInfo.hn',data,function(res){
-      console.log('getCouponInfo',res);
       if(res){
         if(res.success){
-          console.log(res.msg);
           var arr = res.msg;
           let newTime = new Date().getTime();
          for(var i = 0; i< arr.length ;i++){
           var end_time = Date.parse(arr[i].end_time.replace(/-/g, '/'));
           if((end_time-newTime)>0){
-            arr[i].overdueFlag = false
+            arr[i].overdueFlag = false;
           }else{
-            arr[i].overdueFlag = true
+            arr[i].overdueFlag = true;
           }
          }
 
           that.setData({
             couponArr: arr
-          })
+          });
         }else{
         // box.showToast(res.msg);
         }
       }else{
         box.showToast("网络不稳定，请重试");
       }
-    })
+    });
   },
   bindAppointment:function(){
     wx.redirectTo({
       url: '/pages/onsiteAppointment/onsiteAppointment?choose_type=0',
-    })
+    });
   },
   /**
    * Lifecycle function--Called when page is initially rendered
@@ -96,7 +95,7 @@ Page({
    * Called when page reach bottom
    */
   onReachBottom: function () {
-    console.log('111')
+
   },
 
   /**
@@ -110,5 +109,32 @@ Page({
     this.setData({
       number: number
     });
-  }
+    if(number == 1){
+      this.getMineCouponList();
+    }else{
+      this.getCouponList();
+    }
+  },
+  /**
+   * 获取我的优惠卷
+   */
+  getMineCouponList(){
+    var that = this;
+    var data = {
+      user_id: this.data.user_id
+    }
+    request.request_get('/Newacid/getMineCouponList.hn',data,function(res){
+      if(res){
+        if(res.success){
+          that.setData({
+            mineCouponList: res.msg
+          });
+        }else{
+          box.showToast(res.msg);
+        }
+      }else{
+        box.showToast("网络不稳定，请重试");
+      }
+    });
+  },
 })
