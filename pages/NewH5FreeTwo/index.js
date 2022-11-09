5632aa65f9c63e97d8c805f2ce3b2f34fb42cea9
yangqing <yangqing@jiunidu.com>
2022-11-09 10:30:53 +0800

    免费录入 提交接口新增openid参数

--- pages/NewH5FreeTwo/index.js
+++ pages/NewH5FreeTwo/index.js
@@ -81,6 +81,7 @@ Page({
         let params = {
           person_id: this.data.userinfo_id,
           user_id: this.data.user_id,
+          openid: this.data.openid,
         }
         request.request_get('/Newacid/freeSubject.hn', params, function (res) {
           console.info('回调', res)
