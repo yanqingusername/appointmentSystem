4d1562ba56dc1a84207d635512c8898f262eaae3
yangqing <yangqing@jiunidu.com>
2023-02-07 17:18:00 +0800

    version 2.1.5 正式服务器

--- utils/request.js
+++ utils/request.js
@@ -6,10 +6,10 @@
 
 // 参数配置
 // var apiurl = 'http://xmr.coyotebio-lab.com/lis_appointment'    // 测试服务器
-// var apiurl = 'https://cloud.coyotebio-lab.com:8443/lis_appointment'    // 正式服务器
+var apiurl = 'https://cloud.coyotebio-lab.com:8443/lis_appointment'    // 正式服务器
 // var apiurl = "https://scldev.coyotebio-lab.com:8443/lis_appointment"//测试服务器
 
-var apiurl = "https://store.coyotebio-lab.com:8443/lis_appointment"//测试服务器
+// var apiurl = "https://store.coyotebio-lab.com:8443/lis_appointment"//测试服务器
 
 // var apiurl = 'http://syrdev.coyotebio-lab.com:8080/lis_appointment'    // 服务器
 
